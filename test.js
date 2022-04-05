40847d5c1a266a82e0a3ee33af22ac37d77e6567
ProgramWarfare <95079489+ProgramWarfare@users.noreply.github.com>
2022-04-05 19:31:05 +0200

    Add files via upload

--- test.js
+++ test.js
@@ -1,9 +1,32 @@
-"use strict";
-function myFunction() {
-    var x = document.getElementById("myMenyi");
-    if (x.className === "li") {
-      x.className += " responsive";
-    } else {
-      x.className = "li";
-    }
+function Time() {
+  // Creating object of the Date class
+  var date = new Date();
+  // Get current hour
+  var hour = date.getHours();
+  // Get current minute
+  var minute = date.getMinutes();
+  // Get current second
+  var second = date.getSeconds();
+  // Variable to store AM / PM
+  var period = "";
+  // Updating hour, minute, and second
+  // if they are less than 10
+  hour = update(hour);
+  minute = update(minute);
+  second = update(second);
+  // Adding time elements to the div
+  document.getElementById("digital-clock","Digital","Digi","D").innerText = hour + " : " + minute + " : " + second + " " + period;
+  // Set Timer to 1 sec (1000 ms)
+  setTimeout(Time, 1000);
+ }
+  // Function to update time elements if they are less than 10
+  // Append 0 before time elements if they are less than 10
+ function update(t) {
+  if (t < 10) {
+  return "0" + t;
   }
+  else {
+  return t;
+  }
+ }
+ Time();
